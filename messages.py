f26acbdbdd16ac4dd1cf08a46993262904df7505
Waldo Spek <waldospek@gmail.com>
2018-10-24 07:55:09 -0300

    Text content tweaks.

--- messages.py
+++ messages.py
@@ -1,9 +1,11 @@
-start_message = """
+start_message = u"""
 Welcome to the 9GAG media bot.
 
 Use this bot to search 9GAG for pictures, GIFs and video's and forward them to your Telegram friends.
 
 You can make a query in a chat by typing @gagmedia_bot followed by one or more keywords. It's that simple.
+
+If you enjoy this bot, please help me out by rating it through the Store Bot: https://telegram.me/storebot?start=gagmedia_bot 
 
 Have fun!
 """
